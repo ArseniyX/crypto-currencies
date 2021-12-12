1bac6585799e42ca391a2e2333a65c009248c10c
ArseniyX <jkl@usa.com>
2021-12-12 19:34:59 +0200

    local-storage and responsive web design

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const API = {
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false",
   COIN_INFO: "https://api.coingecko.com/api/v3/coins/",
 };
-
 const multiCheckPrice = (currencies) =>
   `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${currencies}&tsyms=USD`;
 
@@ -18,7 +17,7 @@ const UNLOAD = "unload";
 const SEARCH_INPUT = $(".search-input");
 
 // Elements
-const cardElement = ({ name, id, image, symbol }, index) => `
+const cardElement = ({ name, id, symbol }, index) => `
   <div class="card main-card mt-4" style="flex: none;" id="card-${index}">
     <div class="card-body">
       <div class="header-card__container">
@@ -81,7 +80,7 @@ onload = () => {
   } else {
     const data = dataFromStorage();
     createElements(data);
-    $(".spinner-border").hide();
+    $(".spinner").hide();
   }
   createListeners();
 };
@@ -96,14 +95,13 @@ const createListeners = () => {
 
 const onSearchKeyUpListener = (e) => {
   if (e.key === "Enter" || e.keyCode === 13) {
-    // no need for now
   }
   filterElements();
 };
 
 const onHomeTabListener = () => {
   $(CARD_DECK).show();
-  
+
   stopLiveReports();
 };
 
@@ -142,17 +140,51 @@ const isFirstLoading = () => {
   return !firstLoading;
 };
 
+const wasLoaded = (id) => {
+  if (!cashedItems[id]) return true;
+  const seconds = Date.now();
+  return !cashedItems[id];
+};
+
+const compareTime = (id) => {
+  const itemJson = localStorage.getItem(id);
+
+  if (!itemJson) return true;
+
+  const item = JSON.parse(itemJson);
+  const timeNow = Date.now();
+  const timeBefore = +item.timeStamp;
+  const timeDifference = timeNow - timeBefore;
+  if (timeDifference >= 120000) return true;
+  const { index, crypto, image } = item;
+  if (!cashedItems[id]) {
+    $("#collapse-" + index).html(collapseElement(...crypto, image));
+  }
+  return false;
+};
+
 async function onMoreButton(buttonElement) {
   const { id, index } = buttonElement.dataset;
+  const hasAccess = compareTime(id);
 
-  if (!cashedItems[id]) {
+  if (hasAccess) {
     const data = await fetchData(API.COIN_INFO, id);
-    cashedItems = { ...cashedItems, [id]: true };
+    const timeNow = Date.now();
+    localStorage.setItem(id, timeNow.toString());
+    console.log(cashedItems);
+    cashedItems = { ...cashedItems, [id]: !cashedItems[id] };
     const { usd, eur, ils } = data.market_data?.current_price || "";
     let crypto = [usd, eur, ils];
     crypto = crypto.map((el) => numberWithCommas(el));
     const { large: image } = data.image || "";
-
+    const item = {
+      index,
+      crypto,
+      image,
+      timeStamp: timeNow,
+    };
+    const jsonItem = JSON.stringify(item);
+    localStorage.setItem(id, jsonItem);
     $("#collapse-" + index).html(collapseElement(...crypto, image));
   }
 }
@@ -160,14 +192,11 @@ async function onMoreButton(buttonElement) {
 function createElements(data, filteredValue = "") {
   data.forEach((element, index) => {
     if (!filteredValue && isFirstLoad) {
-      console.log("first load");
-
       $(cardElement(element, index)).appendTo(CARD_DECK);
     }
 
     const name = element.name.toLowerCase();
     const symbol = element.symbol.toLowerCase();
-    console.log(symbol);
     if (name.includes(filteredValue) || symbol.includes(filteredValue)) {
       $("#card-" + index).show();
     } else {
@@ -177,11 +206,11 @@ function createElements(data, filteredValue = "") {
   if (isFirstLoad) {
     const moreBtn = $(".more-info").get();
     const toggleBtn = $(".toggle-button");
-    for (let button of moreBtn) {
+    moreBtn.forEach((button) => {
       $(button).on(CLICK, function () {
         onMoreButton(this);
       });
-    }
+    });
     toggleBtn.on(CLICK, function () {
       onToggleButton(this);
     });
@@ -207,7 +236,7 @@ const toggleSmallLoading = (id) => {
 };
 
 function toggleMainLoading() {
-  $(".spinner-border").toggle();
+  $(".spinner").toggle();
 }
 
 const createOptions = () => {
@@ -227,6 +256,7 @@ const createOptions = () => {
   const options = {
     animationEnabled: true,
     theme: "dark1",
+    backgroundColor: "",
     title: {
       text: "Live Reports: " + currencies.join(", "),
     },
@@ -238,6 +268,7 @@ const createOptions = () => {
       crosshair: {
         enabled: true,
       },
+      
     },
     axisX: {
       suffix: "s",
@@ -262,39 +293,39 @@ const createOptions = () => {
 };
 
 const createChart = async () => {
+  toggleMainLoading();
   const data = await getDataFromApi();
   const options = createOptions(data);
   $(CHART).CanvasJSChart(options);
-  addPoints();
+  await renderChart();
+  toggleMainLoading();
 };
 
 const getDataFromApi = () => $.ajax(multiCheckPrice(currencies.join(",")));
 
 const dataToPoint = (data, name, x) => ({ x, y: data[name].USD });
 
-const addPoints = async () => {
+const renderChart = async () => {
   const chart = $(CHART).CanvasJSChart();
   const data = await getDataFromApi();
   chart.options.data.forEach((option) => {
     const point = dataToPoint(data, option.name, seconds);
     option.dataPoints.push(point);
   });
-  chart.render();
+  await chart.render();
   seconds = seconds + 2;
 };
 
 const startLiveReports = () => {
   createChart();
   if (!nIntervalId) {
-    nIntervalId = setInterval(addPoints, 2000);
+    nIntervalId = setInterval(renderChart, 2000);
   }
-
-  console.log("interval started");
 };
 
 const getFromDataset = (arr) => arr.map((el) => el.dataset["symbol"]);
 
-function onToggleButton() {
+const onToggleButton = () => {
   const toggleBtn = $(".toggle-button");
   const checkedButtons = $(".toggle-button:checked").get();
   currencies = getFromDataset(checkedButtons);
@@ -307,105 +338,3 @@ function onToggleButton() {
 $(window).on(UNLOAD, () => {
   localStorage.removeItem(DATA);
 });
-
-var options = {
-  animationEnabled: true,
-  theme: "dark1",
-  title: {
-    text: "Live Reports, 2016-17",
-  },
-  axisY: {
-    includeZero: false,
-    prefix: "$",
-    lineThickness: 0,
-  },
-  toolTip: {
-    shared: true,
-  },
-  legend: {
-    fontSize: 13,
-  },
-  data: [
-    {
-      type: "splineArea",
-      showInLegend: true,
-      name: "Salaries",
-      yValueFormatString: "$#,##0",
-      xValueFormatString: "MMM YYYY",
-      dataPoints: [
-        { x: new Date(2016, 2), y: 28000 },
-        { x: new Date(2016, 3), y: 31500 },
-        { x: new Date(2016, 4), y: 28500 },
-        { x: new Date(2016, 5), y: 30400 },
-        { x: new Date(2016, 6), y: 26900 },
-        { x: new Date(2016, 7), y: 31400 },
-        { x: new Date(2016, 8), y: 31400 },
-        { x: new Date(2016, 9), y: 31000 },
-        { x: new Date(2016, 10), y: 33000 },
-        { x: new Date(2016, 11), y: 35000 },
-        { x: new Date(2017, 0), y: 37900 },
-        { x: new Date(2017, 1), y: 38000 },
-      ],
-    },
-    {
-      type: "splineArea",
-      showInLegend: true,
-      name: "Office Cost",
-      yValueFormatString: "$#,##0",
-      dataPoints: [
-        { x: new Date(2016, 2), y: 18100 },
-        { x: new Date(2016, 3), y: 15000 },
-        { x: new Date(2016, 4), y: 14000 },
-        { x: new Date(2016, 5), y: 18500 },
-        { x: new Date(2016, 6), y: 18500 },
-        { x: new Date(2016, 7), y: 21800 },
-        { x: new Date(2016, 8), y: 20000 },
-        { x: new Date(2016, 9), y: 23000 },
-        { x: new Date(2016, 10), y: 22000 },
-        { x: new Date(2016, 11), y: 24000 },
-        { x: new Date(2017, 0), y: 23000 },
-        { x: new Date(2017, 1), y: 20500 },
-      ],
-    },
-    {
-      type: "splineArea",
-      showInLegend: true,
-      name: "Entertainment",
-      yValueFormatString: "$#,##0",
-      dataPoints: [
-        { x: new Date(2016, 2), y: 13100 },
-        { x: new Date(2016, 3), y: 8000 },
-        { x: new Date(2016, 4), y: 5400 },
-        { x: new Date(2016, 5), y: 4000 },
-        { x: new Date(2016, 6), y: 7000 },
-        { x: new Date(2016, 7), y: 7500 },
-        { x: new Date(2016, 8), y: 6200 },
-        { x: new Date(2016, 9), y: 8500 },
-        { x: new Date(2016, 10), y: 11300 },
-        { x: new Date(2016, 11), y: 12500 },
-        { x: new Date(2017, 0), y: 10500 },
-        { x: new Date(2017, 1), y: 9500 },
-      ],
-    },
-    {
-      type: "splineArea",
-      showInLegend: true,
-      yValueFormatString: "$#,##0",
-      name: "Maintenance",
-      dataPoints: [
-        { x: new Date(2016, 2), y: 1900 },
-        { x: new Date(2016, 3), y: 2300 },
-        { x: new Date(2016, 4), y: 1650 },
-        { x: new Date(2016, 5), y: 1860 },
-        { x: new Date(2016, 6), y: 1200 },
-        { x: new Date(2016, 7), y: 1000 },
-        { x: new Date(2016, 8), y: 1200 },
-        { x: new Date(2016, 9), y: 4500 },
-        { x: new Date(2016, 10), y: 1300 },
-        { x: new Date(2016, 11), y: 3700 },
-        { x: new Date(2017, 0), y: 2700 },
-        { x: new Date(2017, 1), y: 2300 },
-      ],
-    },
-  ],
-};
